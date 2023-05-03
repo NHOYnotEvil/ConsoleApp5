ba0e88ea0f4c79538347aa20b81aa285be9c094d
NHOY <NHOY@2210DR000919>
2023-05-03 12:58:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resourceMoney;
+            int heroHealthMainMenu;
+            int heroDamageMainMenu;
+            int heroAttackSpeedMainMenu;
+            int heroHealthInBattle;
+            int heroDamageInBattle;
+            int heroAttackSpeedInBattle;
+            int enemyHealth;
+            int enemyDamage;
+            int enemyAttackSpeed;
+
+        }
+    }
+}
